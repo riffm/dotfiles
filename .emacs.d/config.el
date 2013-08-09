2f06dc43434123e272680dd7e221d66a86e8e9f9
Tim Perevezentsev <riffm2005@gmail.com>
2013-08-09 23:22:52 +0400

    added python section to emacs config

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -51,6 +51,18 @@
 
 (setq tramp-default-method "ssh")
 
+(defun flymake-pyflakes-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "pyflakes" (list local-file))))
+
+(eval-after-load "flymake"
+  '(add-to-list 'flymake-allowed-file-name-masks
+                '("\\.py\\'" flymake-pyflakes-init)))
+
 (package-initialize)
 
 (defvar emacs-pkgs
