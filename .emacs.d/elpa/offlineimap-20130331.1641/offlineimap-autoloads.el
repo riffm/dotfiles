4d720a1b74431a80504abecf519e7fa40d40a9fd
Tim Perevezentsev <riffm2005@gmail.com>
2013-10-10 17:30:20 +0400

    added offlineimap support to emacs

---
+++ .emacs.d/elpa/offlineimap-20130331.1641/offlineimap-autoloads.el
@@ -0,0 +1,29 @@
+;;; offlineimap-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (offlineimap) "offlineimap" "offlineimap.el" (21078
+;;;;;;  43190 0 0))
+;;; Generated autoloads from offlineimap.el
+
+(autoload 'offlineimap "offlineimap" "\
+Start OfflineIMAP.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("offlineimap-pkg.el") (21078 43190 407526
+;;;;;;  0))
+
+;;;***
+
+(provide 'offlineimap-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; offlineimap-autoloads.el ends here
