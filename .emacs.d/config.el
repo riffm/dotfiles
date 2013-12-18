56517538708c53d84f6682ded613f5488f36d7b4
Tim Perevezentsev <riffm2005@gmail.com>
2013-12-18 22:58:44 +0400

    add ensime init

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -90,6 +90,10 @@
 
 (require 'ahg)
 
+(add-to-list 'load-path "~/dotfiles/ensime_2.10.0-0.9.8.9/elisp/")
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
 (setq message-send-mail-function 'message-send-mail-with-sendmail
       sendmail-program "msmtp"
       user-full-name "Tim Perevezentsev")
