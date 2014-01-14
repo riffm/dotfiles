e508db956f7b6475f211ccb20cf522570b331390
Tim Perevezentsev <riffm2005@gmail.com>
2014-01-14 15:22:21 +0400

    updated some pkgs

--- .emacs.d/elpa/jabber-20140108.1737/jabber-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "jabber" "20140108.1737" "A Jabber client for Emacs." 'nil)
---
+++ .emacs.d/elpa/slime-20140113.900/slime-pkg.el
@@ -0,0 +1,2 @@
+(define-package "slime" "20140113.900" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.3")))
