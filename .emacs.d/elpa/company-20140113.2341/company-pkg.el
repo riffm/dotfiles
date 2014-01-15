09e77766ed92b280188a00257f949dac19f22165
Tim Perevezentsev <riffm2005@gmail.com>
2014-01-15 20:54:56 +0400

    updated some pkgs

--- .emacs.d/elpa/company-20140113.2341/company-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "company" "20140113.2341" "Modular in-buffer completion framework" 'nil)
---
+++ .emacs.d/elpa/company-20140114.1945/company-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company" "20140114.1945" "Modular in-buffer completion framework" 'nil)
