86382a7673766731f98b4735bf099bef79a1f806
Tim Perevezentsev <riffm2005@gmail.com>
2014-03-05 16:50:27 +0400

    pkgs update

---
+++ .emacs.d/elpa/company-20140304.2155/company-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company" "20140304.2155" "Modular in-buffer completion framework" 'nil)
---
+++ .emacs.d/elpa/epl-20140211.524/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21271 7459 688063
+;;;;;;  0))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
