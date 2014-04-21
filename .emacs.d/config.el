171301bc81eb1cb20c58c135edf9e79e2b0f1d61
Tim Perevezentsev <riffm2005@gmail.com>
2014-04-21 14:14:47 +0400

    installed flycheck

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -30,16 +30,6 @@
       (message "%s" file)
       (delete-file file))))
 
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-
-(eval-after-load "flymake"
-  '(progn
-     (local-set-key "\M-n" 'flymake-goto-next-error)
-     (local-set-key "\M-p" 'flymake-goto-prev-error)))
-
-(setf flymake-no-changes-timeout 20
-      flymake-start-syntax-check-on-newline nil)
-
 (setq-default indent-tabs-mode nil)
 (setq-default indicate-empty-lines t)
 (setq show-trailing-whitespace t)
@@ -132,7 +122,22 @@
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-(add-hook 'haskell-mode-hook 'flymake-haskell-multi-load)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-x C-d") nil)
+     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c M-.") nil)
+     (define-key haskell-mode-map (kbd "C-c C-d") nil)
+     (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile)))
+
+(eval-after-load "haskell-cabal"
+    '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
 
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
@@ -210,3 +215,8 @@
 
 (setq message-sendmail-envelope-from 'header)
 (add-hook 'message-send-mail-hook 'choose-msmtp-account)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
