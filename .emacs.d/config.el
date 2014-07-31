a96670cd694cedb2158556742835753511aef80f
Tim Perevezentsev <riffm2005@gmail.com>
2014-07-31 18:03:08 +0400

    do not ask about babel src evaluation

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -225,12 +225,30 @@
 (setq org-log-done 'time)
 (define-key mode-specific-map [?a] 'org-agenda)
 (custom-set-variables
- '(org-agenda-files (quote ("~/Dropbox/tasks.org")))
+ '(org-agenda-files (quote ("~/Dropbox/tasks.org"
+                            "~/Dropbox/tasks.org_archive")))
  '(org-default-notes-file "~/Dropbox/notes.org")
  '(org-agenda-show-all-dates t)
  '(org-agenda-start-on-weekday nil)
  '(org-deadline-warning-days 14)
  '(org-agenda-show-all-dates t)
+ '(org-agenda-custom-commands
+   (quote (("d" todo "DELEGATED" nil)
+           ("c" todo "DONE|DEFERRED|CANCELLED" nil)
+           ("w" todo "WAITING" nil)
+           ("W" agenda "" ((org-agenda-ndays 21)))
+           ("A" agenda ""
+            ((org-agenda-skip-function
+              (lambda nil
+                (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
+             (org-agenda-ndays 1)
+             (org-agenda-overriding-header "Today's Priority #A tasks: ")))
+           ("u" alltodo ""
+            ((org-agenda-skip-function
+              (lambda nil
+                (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
+                                          (quote regexp) "\n]+>")))
+             (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
  '(org-agenda-skip-deadline-if-done t)
       '(org-agenda-skip-scheduled-if-done t))
 
@@ -245,7 +263,8 @@
    (plantuml . t)))
 
 (custom-set-variables
- '(org-plantuml-jar-path "~/Dropbox/bin/plantuml.jar"))
+ '(org-plantuml-jar-path "~/Dropbox/bin/plantuml.jar")
+ '(org-confirm-babel-evaluate nil))
 
 (add-to-list 'org-src-lang-modes (quote ("plantuml" . fundamental)))
 
